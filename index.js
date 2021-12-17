f3822abcb3b0580cafe62593172c962e403218f9
Griffin Hughes <Griffinhughes2020@gmail.com>
2021-12-17 16:54:14 -0500

    commit tree 887d34ebfc08adfe144cf5362a3ad9c5a3a6ada2

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser ='bob';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = toUpperCase(welcomeMessage);
